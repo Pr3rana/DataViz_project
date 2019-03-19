bbcc3d2f685558f29bbfffe6a07bddc3d78defa7
Prerana Singh <prernasingh726@gmail.com>
2019-03-20 02:00:51 +0530

    Added comments

--- js/index.js
+++ js/index.js
@@ -21,24 +21,24 @@ function processData(data) {
     });
     addList(newData);
     locationCenter = getLocationCenter(newData);
-    drawMap(newData, locationCenter)
+    drawMap(newData, locationCenter);
 }
 
 //add list to assets
 function addList(newData) {
-  for (let i = 0; i < newData.length; i++) {
-      let txt = newData[i].reference;
-      let containerDiv = document.createElement("div");
-      containerDiv.setAttribute("class", "container list-group-item list-group-item-action list-group-item-light col-sm-12");
-      let input = document.createElement("input");
-      input.setAttribute("type", "checkbox");
-      input.setAttribute("class", "checkbox");
-      let label = document.createElement("label");
-      label.innerText = txt;
-      containerDiv.appendChild(input);
-      containerDiv.appendChild(label);
-      document.getElementById('list').appendChild(containerDiv);
-  }
+    for (let i = 0; i < newData.length; i++) {
+        let txt = newData[i].reference;
+        let containerDiv = document.createElement("div");
+        containerDiv.setAttribute("class", "container list-group-item list-group-item-action list-group-item-light col-sm-12");
+        let input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("class", "checkbox");
+        let label = document.createElement("label");
+        label.innerText = txt;
+        containerDiv.appendChild(input);
+        containerDiv.appendChild(label);
+        document.getElementById('list').appendChild(containerDiv);
+    }
 
 }
 //find the center of multiple co-ordinate
@@ -97,7 +97,7 @@ function drawMap(newData, locationCenter) {
         google.maps.event.addListener(marker, 'click', showChart.bind(null, ttl));
     }
 };
-
+//show chart when red marker got clicked
 function showChart(ttl) {
     var clickedMarkerInfo = [];
     for (let i = 0; i < newData.length; i++) {
@@ -143,189 +143,184 @@ function applyFilter() {
 
 //get risk data
 function getRiskData(filteredCords) {
-  var riskDataObj = {},
-      minMax = {};
-  for (let i = 0; i < filteredCords.length; i++) {
-      let allVal = [],
-          category = [],
-          data = [];
-      const element = filteredCords[i];
-      var key = element.reference;
-      riskDataObj[key] = [];
-      for (let value in element) {
-          if (value != "reference" && value != "latitude" && value != "longitude") {
-              let displayValue = (element[value])
-              allVal.push(element[value])
-              category.push({
-                  "label": value
-              });
-              data.push({
-                  "displayValue": element[value]
-              });
-          }
-      }
-      var normData = getNormalizeVal(allVal,category,data);
-      
-      riskDataObj[key].push(normData.jsonObj)
-      let avg = (normData.normMin + normData.normMax) / 2;
-      minMax[key] = {
-          "min": normData.normMin,
-          "max": normData.normMax,
-          "avg": avg
-      };
-  }
+    var riskDataObj = {},
+        minMax = {};
+    for (let i = 0; i < filteredCords.length; i++) {
+        let allVal = [],
+            category = [],
+            data = [];
+        const element = filteredCords[i];
+        var key = element.reference;
+        riskDataObj[key] = [];
+        for (let value in element) {
+            if (value != "reference" && value != "latitude" && value != "longitude") {
+                let displayValue = (element[value])
+                allVal.push(element[value])
+                category.push({
+                    "label": value
+                });
+                data.push({
+                    "displayValue": element[value]
+                });
+            }
+        }
+        var normData = getNormalizeVal(allVal, category, data);
 
-  return [riskDataObj, minMax];
+        riskDataObj[key].push(normData.jsonObj)
+        let avg = (normData.normMin + normData.normMax) / 2;
+        minMax[key] = {
+            "min": normData.normMin,
+            "max": normData.normMax,
+            "avg": avg
+        };
+    }
+
+    return [riskDataObj, minMax];
 }
 //get normalize data
-function getNormalizeVal(allVal,category,data) {
-  let min = Math.min(...allVal),
-      max = Math.max(...allVal),
-      allNormVal = [],
-      normVal,
-      normData={};
-  for (let j = 0; j < data.length; j++) {
-      let val = data[j].displayValue;
-      if(max==min){
-        normVal = 1/data.length;
-      }
-      else{
-        normVal = (val - min) / (max - min);
-      }
-      allNormVal.push(normVal);
-      data[j].value = normVal;
-  }
-  let normMin = Math.min(...allNormVal);
-  let normMax = Math.max(...allNormVal);
-  console.log(allNormVal,normMin,normMax,"norm");
-  normData.jsonObj = {
-      "category": category,
-      "data": data
-  };
-  normData.normMin = normMin;
-  normData.normMax = normMax;
-  return normData;
+function getNormalizeVal(allVal, category, data) {
+    let min = Math.min(...allVal),
+        max = Math.max(...allVal),
+        allNormVal = [],
+        normVal,
+        normData = {};
+    for (let j = 0; j < data.length; j++) {
+        let val = data[j].displayValue;
+        if (max == min) {
+            normVal = 1 / data.length;
+        } else {
+            normVal = (val - min) / (max - min);
+        }
+        allNormVal.push(normVal);
+        data[j].value = normVal;
+    }
+    let normMin = Math.min(...allNormVal);
+    let normMax = Math.max(...allNormVal);
+    normData.jsonObj = {
+        "category": category,
+        "data": data
+    };
+    normData.normMin = normMin;
+    normData.normMax = normMax;
+    return normData;
 };
-//highlight specic markers
+//highlight filtered markers
 function startBounce(filteredCords, newCenter, riskData, minMax) {
-  map = new google.maps.Map(document.getElementById('map_canvas'), {
-      zoom: 12,
-      center: new google.maps.LatLng(newCenter[0], newCenter[1]),
-      mapTypeId: google.maps.MapTypeId.SATELLITE
-  });
-  for (let i = 0; i < filteredCords.length; i++) {
-      var lat = filteredCords[i].latitude;
-      var long = filteredCords[i].longitude;
-      var title = filteredCords[i].reference;
-      marker = new google.maps.Marker({
-          animation: google.maps.Animation.BOUNCE,
-          position: new google.maps.LatLng(lat, long),
-          icon: {
-              url: "./assets/placeholder.png"
-          },
-          title: title,
-          map: map
-      });
-      window.google.maps.event.addListener(marker, 'click', openModal.bind(null, riskData, minMax, title));
-  }
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+        zoom: 12,
+        center: new google.maps.LatLng(newCenter[0], newCenter[1]),
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+    for (let i = 0; i < filteredCords.length; i++) {
+        var lat = filteredCords[i].latitude;
+        var long = filteredCords[i].longitude;
+        var title = filteredCords[i].reference;
+        marker = new google.maps.Marker({
+            animation: google.maps.Animation.BOUNCE,
+            position: new google.maps.LatLng(lat, long),
+            icon: {
+                url: "./assets/placeholder.png"
+            },
+            title: title,
+            map: map
+        });
+        window.google.maps.event.addListener(marker, 'click', openModal.bind(null, riskData, minMax, title));
+    }
 }
 //open modal for rendering chats
 function openModal(riskData, minMax, title) {
-  modal = document.getElementById('myModal');
-  var close = document.getElementById("close");
+    modal = document.getElementById('myModal');
+    var close = document.getElementById("close");
 
-  for (let key in riskData) {
-      if (key == title) {
-          let min = minMax[key].min;
-          let max = minMax[key].max;
-          let avg = minMax[key].avg;
-          renderChart(key, riskData[key], min, max, avg);
-      }
-  }
-  modal.style.display = "block";
+    for (let key in riskData) {
+        if (key == title) {
+            let min = minMax[key].min;
+            let max = minMax[key].max;
+            let avg = minMax[key].avg;
+            renderChart(key, riskData[key], min, max, avg);
+        }
+    }
+    modal.style.display = "block";
 
-  close.addEventListener("click", function() {
-      modal.style.display = "none";
-  })
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
+    close.addEventListener("click", function() {
+        modal.style.display = "none";
+    })
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
-
+//render the charts
 function renderChart(title, riskData, min, max, avg) {
-console.log(riskData, min, max, avg, "data")
-  let newMin = min - avg / 2;
-  var dataSrc = {
-      "caption": "Risk data for " + title,
-      "xAxisName": title,
-      "yAxisName": "Risk probability",
-      //"numberSuffix": "%",
-      "numVisiblePlot": "12",
-      //"palettecolors":"#5E8DDC",
-      //"exportEnabled":"1",
-      "decimals": "8",
-      "yAxisMinvalue": newMin,
-      "yAxisMaxvalue": max,
-      "plotToolText": "timestamp: $label <br> Risk Value: $displayValue <br> Normalize Value: $dataValue",
-      "theme": "fusion",
-  };
-  if (min == max) {
-    dataSrc.yAxisMinvalue = min - avg;
-    dataSrc.yAxisMaxvalue = max + avg;
-  } else {
-    dataSrc.yAxisMinvalue = newMin;
-    dataSrc.yAxisMaxvalue = max;
-  }
-  var chartInstance = new FusionCharts({
-      type: 'scrollcolumn2d',
-      width: "100%", // Width of the chart
-      height: "100%", // Height of the chart
-      dataFormat: 'json', // Data type
-      renderAt: 'chart-container', //container where the chart will render
-      dataSource: {
-          "chart": dataSrc,
-          "categories": [{
-              "category": riskData[0].category,
-          }],
-          // Chart Data
-          "dataset": [{
-              "data": riskData[0].data
-          }]
-      }
-  });
-  // Render
-  chartInstance.render();
+    let newMin = min - avg / 2;
+    var dataSrc = {
+        "caption": "Risk data for " + title,
+        "xAxisName": title,
+        "yAxisName": "Normalize Risk Probability",
+        "numVisiblePlot": "12",
+        "decimals": "8",
+        "yAxisMinvalue": newMin,
+        "yAxisMaxvalue": max,
+        "plotToolText": "timestamp: $label <br> Risk Value: $displayValue <br> Normalize Value: $dataValue",
+        "theme": "fusion",
+    };
+    if (min == max) {
+        dataSrc.yAxisMinvalue = min - avg;
+        dataSrc.yAxisMaxvalue = max + avg;
+    } else {
+        dataSrc.yAxisMinvalue = newMin;
+        dataSrc.yAxisMaxvalue = max;
+    }
+    var chartInstance = new FusionCharts({
+        type: 'scrollcolumn2d',
+        width: "100%", // Width of the chart
+        height: "100%", // Height of the chart
+        dataFormat: 'json', // Data type
+        renderAt: 'chart-container', //container where the chart will render
+        dataSource: {
+            "chart": dataSrc,
+            "categories": [{
+                "category": riskData[0].category,
+            }],
+            // Chart Data
+            "dataset": [{
+                "data": riskData[0].data
+            }]
+        }
+    });
+    // Render
+    chartInstance.render();
 }
 
 //apply the filter
-function applyFilter(){
-  var checkedArr = [], filteredCords=[];
-  document.querySelectorAll('.container').forEach(el=>{
-    if(el.children[0].checked){
-      checkedArr.push(el.children[1].innerText);
+function applyFilter() {
+    var checkedArr = [],
+        filteredCords = [];
+    document.querySelectorAll('.container').forEach(el => {
+        if (el.children[0].checked) {
+            checkedArr.push(el.children[1].innerText);
+        }
+    })
+    if (checkedArr.length == 0) {
+        alert("No asset has been selected!");
+        return;
     }
-  })
-  if (checkedArr.length==0) {
-    alert("No asset has been selected!");
-    return;
-  }
-  for (let i = 0; i < checkedArr.length; i++) {
-    var el1 = checkedArr[i];
-    for (let j = 0; j < newData.length; j++) {
-      var el2 = newData[j];
-      if (el2.reference==el1) {
-        filteredCords.push(el2);
-      }
+    for (let i = 0; i < checkedArr.length; i++) {
+        var el1 = checkedArr[i];
+        for (let j = 0; j < newData.length; j++) {
+            var el2 = newData[j];
+            if (el2.reference == el1) {
+                filteredCords.push(el2);
+            }
+        }
     }
-  }
-  var data = getRiskData(filteredCords);
-  var riskData = data[0];
-  var minMax = data[1];
-  var newCenter = getLocationCenter(filteredCords);
-  modal = document.getElementById('myModal').style.display = "none";
-  startBounce(filteredCords,newCenter,riskData,minMax);
+    var data = getRiskData(filteredCords);
+    var riskData = data[0];
+    var minMax = data[1];
+    var newCenter = getLocationCenter(filteredCords);
+    modal = document.getElementById('myModal').style.display = "none";
+    startBounce(filteredCords, newCenter, riskData, minMax);
 }
 //remove existing filter
 function removeFilter() {
@@ -342,23 +337,23 @@ function removeFilter() {
 
 //Search out required data
 function search() {
-  var input, filter, ul, div, label, i, txtValue;
-  var sadFace = document.getElementById('sadFace');
-  input = document.getElementById("input");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("list");
-  div = ul.getElementsByTagName("div");
-  for (i = 0; i < div.length; i++) {
-      label = div[i].getElementsByTagName("label")[0];
-      txtValue = label.textContent || label.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          div[i].style.display = "";
-      } else {
-          div[i].style.display = "none";
-      }
-  }
+    var input, filter, ul, div, label, i, txtValue;
+    var sadFace = document.getElementById('sadFace');
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("list");
+    div = ul.getElementsByTagName("div");
+    for (i = 0; i < div.length; i++) {
+        label = div[i].getElementsByTagName("label")[0];
+        txtValue = label.textContent || label.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            div[i].style.display = "";
+        } else {
+            div[i].style.display = "none";
+        }
+    }
 }
-
+//add listner to required elements
 document.getElementById("apply").addEventListener('click', applyFilter);
 document.getElementById("reset").addEventListener('click', removeFilter);
 document.getElementById("input").addEventListener("keyup", search);
